1b0c7bc22ef739add2191f26e671cee26b36d89d
Anze <anze603@gmail.com>
2021-12-28 13:05:59 +0100

    config

--- src/main/app.module.ts
+++ src/main/app.module.ts
@@ -5,16 +5,21 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Reservation } from 'src/reservation/entities/reservation.entity';
 import { ReservationModule } from 'src/reservation/reservation.module';
+import config from 'src/config/config';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
+    //CONFIG LOADING DISABLED GITHUB ACTIONS DOCKER BUILD BUG
+    ConfigModule.forRoot({ isGlobal: true /*, load: [config] */ }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: async (configService: ConfigService) => ({
         type: 'postgres',
-        url: configService.get<string>('datasources.connection-url'),
+        url: configService.get<string>(
+          'datasources_connection_url',
+          configService.get<string>('datasources.connection-url'),
+        ),
         synchronize: true,
         logging: false,
         entities: [Reservation],
